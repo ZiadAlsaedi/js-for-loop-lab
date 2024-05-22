dec0fa4c0e549b1646f39d44f71ebc5a0511bd1d
ZiadAlsaedi <ziad12399@gmail.com>
2024-05-22 17:39:50 +0300

    v2

--- main.js
+++ main.js
@@ -25,3 +25,10 @@ for (let i = 1; i <= 10; i++) {
 }
 
 //5
+for (var i = 1; i <= 3; i++) {
+  for (var j = 1; j <= 10; j++) {
+      console.log(i + " x " + j + " = " + (i * j));
+  }
+  console.log(); 
+}
+
